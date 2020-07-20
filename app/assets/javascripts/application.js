f77ee6335c21e623f7de3f72f9415a6bf1148575
GameOra <106124753+GameOra@users.noreply.github.com>
2020-07-20 10:44:12 +0000

    Attempt #2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,5 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
-// Any JavaScript/Coffee file within this directory, lib/assets/ja
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced
