a9bad234af8ca0f0e050c49654df8145c950ab90
GameOra <106124753+GameOra@users.noreply.github.com>
2020-07-20 09:37:57 +0000

    And again

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,4 @@
-// This is a manifest file that'll be compiled into application.js, 
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/ja
