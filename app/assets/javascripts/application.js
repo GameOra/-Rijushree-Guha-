ad8d768cfaed0bd34b2ae64ca889d07b66864c00
GameOra <106124753+GameOra@users.noreply.github.com>
2020-07-20 18:44:53 +0000

    Sync with Yosemite.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,4 +2,6 @@
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll 
