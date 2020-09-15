202fa81a1d243358bc9941e9d51f26a26130285b
GameOra <106124753+GameOra@users.noreply.github.com>
2020-09-15 06:24:47 +0000

    release 0.3.3

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,4 +6,8 @@ class Ability
   def initialize(user)
     @user = user
     if user
-      if Role.pluck(:name).include?(user.roles.first.try(:name)
+      if Role.pluck(:name).include?(user.roles.first.try(:name))
+        send("#{user.roles.first.name}_abilities")
+      else
+        guest_abilities
+      e
