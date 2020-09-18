5c67906a583df5be2521fe72f6c8fc96945b6119
GameOra <106124753+GameOra@users.noreply.github.com>
2020-09-18 14:38:18 +0000

    Travis!

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,12 @@ class Item < ApplicationRecord
   before_save :calculate_total_wieght
 
   validates :quantity, presence: true
-  validates_numericality_of :quantity, greater_than_or_equal_to: 
+  validates_numericality_of :quantity, greater_than_or_equal_to: 0
+
+  self.per_page = 10
+
+  def add(amount)
+    self.quantity = self.quantity.to_i + amount.to_i
+  end
+
+  def subtract(am
