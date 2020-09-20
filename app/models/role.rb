b437f259c72a65713d272d9da48e05cafbb9da0a
GameOra <106124753+GameOra@users.noreply.github.com>
2020-09-20 12:19:26 +0000

    schema fix

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,8 @@
 class Role < ApplicationRecord
-  has_and_belongs_to_many :users, :join_table 
+  has_and_belongs_to_many :users, :join_table => :users_roles
+
+  belongs_to :resource,
+             :polymorphic => true,
+             :optional => true
+
+  
