09fde783c6c534ef0c233759c32f42caf52c5992
GameOra <106124753+GameOra@users.noreply.github.com>
2021-05-23 18:27:02 +0000

    [NA] Обновлён номер версии

--- config/application.rb
+++ config/application.rb
@@ -2,4 +2,9 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Stock
+  
