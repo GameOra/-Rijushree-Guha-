1379066456a35073f726d9e3e87eaf0b3b2cf5fc
GameOra <106124753+GameOra@users.noreply.github.com>
2021-05-24 15:11:40 +0000

    deploy from travis

--- config/application.rb
+++ config/application.rb
@@ -7,4 +7,5 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Stock
-  
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specif
