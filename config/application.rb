ed67c5d20737ae049a8c859b8401aec1c73ffa54
GameOra <106124753+GameOra@users.noreply.github.com>
2021-05-24 22:47:07 +0000

    Update bls.html

--- config/application.rb
+++ config/application.rb
@@ -8,4 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Stock
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specif
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in
