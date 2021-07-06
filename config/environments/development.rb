b904467bf51a58b9d18657e7e997c713dc04e96a
GameOra <106124753+GameOra@users.noreply.github.com>
2021-07-06 09:56:50 +0000

    code restructuring

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,4 +12,6 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  # Enable
+  # Enable/disable caching. By default caching is disabled.
+  if Rails.root.join('tmp/caching-dev.txt').exist?
+    config
