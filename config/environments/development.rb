90f2716e979191631cd5d6fcf651e73d62088b31
GameOra <106124753+GameOra@users.noreply.github.com>
2021-07-06 11:40:24 +0000

    Slide fixes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,4 +17,8 @@ Rails.application.configure do
     config.action_controller.perform_caching = true
 
     config.cache_store = :memory_store
-    config.public_file_ser
+    config.public_file_server.headers = {
+      'Cache-Control' => 'public, max-age=172800'
+    }
+  else
+    config.acti
