fd4cfa781ce0abd5e2191a7bd5faa2a0d553f0d2
GameOra <106124753+GameOra@users.noreply.github.com>
2021-07-06 22:45:35 +0000

    Cambios varios

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,3 +35,6 @@ Rails.application.configure do
   config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations.
+  config.active_record.migration_error = :page_load
+
+  # Debug mode disables concatenation and preprocess
