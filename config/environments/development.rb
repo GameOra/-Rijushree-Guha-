77fb0d545e4fd53c3e2c5de7424bce037b585199
GameOra <106124753+GameOra@users.noreply.github.com>
2021-07-16 18:14:45 +0000

    adreno_idler: fix-up some comments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,4 +48,6 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  # Use an evented file watcher to asynchronously detect changes in source code
+  # Use an evented file watcher to asynchronously detect changes in source code,
+  # routes, locales, etc. This feature depends on the listen gem.
+  config.file_watcher
