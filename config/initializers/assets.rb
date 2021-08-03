377b758ac3d4e7f849f912dce64e6864c3b4def4
GameOra <106124753+GameOra@users.noreply.github.com>
2021-08-03 22:06:16 +0000

    Precompile assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,4 +6,5 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
-# Precompile additi
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
