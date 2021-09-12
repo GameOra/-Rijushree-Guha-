a2e5df2c0e2c2976db400dadf2a6612f69c84123
GameOra <106124753+GameOra@users.noreply.github.com>
2021-09-12 19:11:18 +0000

    revert test

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -1,3 +1,7 @@
 # Be sure to restart your server when you modify this file.
 #
-# This file contains migration options to ease your Rails 5
+# This file contains migration options to ease your Rails 5.0 upgrade.
+#
+# Read the Guide for Upgrading Ruby on Rails for more info on each option.
+
+Rails.applicat
