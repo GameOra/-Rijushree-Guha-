09327fe4a43bc7d44d0084f30bb8a32014c50bf3
GameOra <106124753+GameOra@users.noreply.github.com>
2021-09-16 21:03:57 +0000

    modify text

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -14,4 +14,7 @@ Rails.application.config.action_controller.forgery_protection_origin_check = tru
 
 # Make Ruby 2.4 preserve the timezone of the receiver when calling `to_time`.
 # Previous versions had false.
-ActiveSuppor
+ActiveSupport.to_time_preserves_timezone = true
+
+# Require `belongs_to` associations by default. Previous versions had false.
+Rails.
