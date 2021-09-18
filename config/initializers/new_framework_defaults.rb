b411028efe274b5637647c76dadf4a1940f17a8e
GameOra <106124753+GameOra@users.noreply.github.com>
2021-09-18 11:50:12 +0000

    1.5.9

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -17,4 +17,6 @@ Rails.application.config.action_controller.forgery_protection_origin_check = tru
 ActiveSupport.to_time_preserves_timezone = true
 
 # Require `belongs_to` associations by default. Previous versions had false.
-Rails.
+Rails.application.config.active_record.belongs_to_required_by_default = true
+
+# Do not halt call
