16e3192be577d418f0f56c3ce28739ff70126405
GameOra <106124753+GameOra@users.noreply.github.com>
2021-09-20 08:04:49 +0000

    added some style

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -19,4 +19,5 @@ ActiveSupport.to_time_preserves_timezone = true
 # Require `belongs_to` associations by default. Previous versions had false.
 Rails.application.config.active_record.belongs_to_required_by_default = true
 
-# Do not halt call
+# Do not halt callback chains when a callback returns false. Previous versions had true.
+ActiveSupport.
