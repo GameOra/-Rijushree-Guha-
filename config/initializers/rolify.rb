f3d5a90d0b5ab2859969154edd24ca5dd5ab4e5e
GameOra <106124753+GameOra@users.noreply.github.com>
2021-09-21 12:09:14 +0000

    Update convert.jl

---
+++ config/initializers/rolify.rb
@@ -0,0 +1,6 @@
+
+Rolify.configure do |config|
+  # By default ORM adapter is ActiveRecord. uncomment to use mongoid
+  # config.use_mongoid
+
+  # Dynamic shortcuts for User class (user.is_admin? like methods). Default is: false
