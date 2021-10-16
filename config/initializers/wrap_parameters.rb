6dd0589d60aa3bd74c335a9d3cf2c8c4882169d3
GameOra <106124753+GameOra@users.noreply.github.com>
2021-10-16 14:26:28 +0000

    Update .github/settings.yml

--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -5,4 +5,8 @@
 
 # Enable parameter wrapping for JSON. You can disable this by setting :format to an empty array.
 ActiveSupport.on_load(:action_controller) do
-  wrap_parameter
+  wrap_parameters format: [:json]
+end
+
+# To enable root element in JSON for ActiveRecord objects.
+# ActiveSupport.on_load(
