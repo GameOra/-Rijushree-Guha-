d49a8f792d28885c2de6c414dc8ada4093fa26b3
GameOra <106124753+GameOra@users.noreply.github.com>
2022-04-02 18:12:58 +0000

    ASoC: msm: qdsp6v2: add error handling on write done during partial drain

--- config/puma.rb
+++ config/puma.rb
@@ -2,4 +2,7 @@
 # The `threads` method setting takes two numbers a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum, this matches the default thread 
+# and maximum, this matches the default thread size of Active Record.
+#
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
+threads threads_cou
