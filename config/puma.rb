19e5ab3454f1bca5b2205dd5449bce2aafac1ea8
GameOra <106124753+GameOra@users.noreply.github.com>
2022-05-01 19:47:34 +0000

    Removes console.log

--- config/puma.rb
+++ config/puma.rb
@@ -24,4 +24,5 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
-# This direct
+# This directive tells Puma to first boot the application and load code
+# before forking the application. This takes ad
