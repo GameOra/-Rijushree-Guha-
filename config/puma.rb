b55b43d25091c4d1d2026d623f0988373c6f59b7
GameOra <106124753+GameOra@users.noreply.github.com>
2022-05-01 10:20:00 +0000

    commit update

--- config/puma.rb
+++ config/puma.rb
@@ -21,4 +21,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-# workers ENV.fetch("WEB_CONCURRENCY
+# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+
+# Use the `preload_app!` method when specifying a `workers` number.
+# This direct
