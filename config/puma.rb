a286dd987e6c8a7269a262959174269f993def0f
GameOra <106124753+GameOra@users.noreply.github.com>
2022-05-08 19:16:49 +0000

    Version 1.1r37

--- config/puma.rb
+++ config/puma.rb
@@ -34,4 +34,5 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # The code in the `on_worker_boot` will be called if you are using
 # clustered mode by specifying a number of `workers`. After each worker
-# process is booted this block will be run, if y
+# process is booted this block will be run, if you are using `preload_app!`
+# option you will want to use this block to reconnect to any threads
