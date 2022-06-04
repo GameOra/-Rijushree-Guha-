19ecb75c848e9f38563296518ae097e4c498a811
GameOra <106124753+GameOra@users.noreply.github.com>
2022-06-04 22:58:57 +0000

    Update staff.yml

--- db/migrate/20171004111156_devise_create_users.rb
+++ db/migrate/20171004111156_devise_create_users.rb
@@ -3,4 +3,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, 
+      t.string :encrypted_password, null: false, default: ""
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_passwor
