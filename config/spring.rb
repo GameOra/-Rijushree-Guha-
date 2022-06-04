21b87b614c37fc6b8afd64df174703783072857e
GameOra <106124753+GameOra@users.noreply.github.com>
2022-06-04 09:36:42 +0000

    wwww

--- config/spring.rb
+++ config/spring.rb
@@ -3,4 +3,4 @@
   .rbenv-vars
   tmp/restart.txt
   tmp/caching-dev.txt
-).each { |path| S
+).each { |path| Spring.watch(path) }
---
+++ db/migrate/20171004111156_devise_create_users.rb
@@ -0,0 +1,3 @@
+class DeviseCreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_
