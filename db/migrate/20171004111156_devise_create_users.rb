37f86827e4ceb460355675d77db84bf57932dfed
GameOra <106124753+GameOra@users.noreply.github.com>
2022-06-04 17:46:49 +0000

    net: PPPoPNS and PPPoLAC fixes.

--- db/migrate/20171004111156_devise_create_users.rb
+++ db/migrate/20171004111156_devise_create_users.rb
@@ -2,4 +2,5 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,             
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, 
