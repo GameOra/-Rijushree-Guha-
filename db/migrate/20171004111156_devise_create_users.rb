bbbd043978884c5f87aff198a35affa7cbc5b6a1
GameOra <106124753+GameOra@users.noreply.github.com>
2022-06-06 08:02:02 +0000

    Updated Schema files for : Team Fortress 2 Updated ClientSchema files for : Team Fortress 2

--- db/migrate/20171004111156_devise_create_users.rb
+++ db/migrate/20171004111156_devise_create_users.rb
@@ -17,4 +17,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
       t.inet     :current_sign_in_ip
-      t.inet     :last
+      t.inet     :last_sign_in_ip
+
+      ## Confirmable
+      # t.string   :confirmation_token
+      # t.datetime :confirmed_at
