21b6f650bc04fea4bf71011c41583ee696579991
GameOra <106124753+GameOra@users.noreply.github.com>
2022-06-10 13:27:07 +0000

    Add sitemap.xml

--- db/migrate/20171004111156_devise_create_users.rb
+++ db/migrate/20171004111156_devise_create_users.rb
@@ -26,4 +26,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       # t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
-      # t.integer  :failed_attempts, default: 0, null: false # Only if lock
+      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
+      # t.string   :unlock_token # Only if unlock strategy is :email or :both
+      # t.da
