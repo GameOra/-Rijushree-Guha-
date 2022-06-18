905b8a1b949045c2b897cc0ca7d15b85bd60172d
GameOra <106124753+GameOra@users.noreply.github.com>
2022-06-18 06:17:13 +0000

    IEATTA web app.

--- db/migrate/20171004121320_create_hangars.rb
+++ db/migrate/20171004121320_create_hangars.rb
@@ -1,3 +1,9 @@
 class CreateHangars < ActiveRecord::Migration[5.0]
   def change
-    create_table :hangars 
+    create_table :hangars do |t|
+      t.integer :number
+
+      t.timestamps
+    end
+  end
+end
