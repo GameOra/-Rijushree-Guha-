59c4b60f7d6d6082edcb23af1ff2d856ecee72e3
GameOra <106124753+GameOra@users.noreply.github.com>
2022-06-18 14:34:57 +0000

    test report

--- db/migrate/20171004121436_create_products.rb
+++ db/migrate/20171004121436_create_products.rb
@@ -1,4 +1,8 @@
 class CreateProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :products do |t|
-      t
+      t.string :name
+      t.decimal :wieght
+      t.references :hangar, foreign_key: true
+
+      t.times
