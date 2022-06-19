d0931000c4e14b9b8bf18d86eb3cfc9530dae105
GameOra <106124753+GameOra@users.noreply.github.com>
2022-06-19 15:51:53 +0000

    fix flake

--- db/migrate/20171006052933_create_items.rb
+++ db/migrate/20171006052933_create_items.rb
@@ -1,3 +1,6 @@
 class CreateItems < ActiveRecord::Migration[5.0]
   def change
-    create_tabl
+    create_table :items do |t|
+      t.references :product
+      t.decimal :total_wieght
+      t.integer
