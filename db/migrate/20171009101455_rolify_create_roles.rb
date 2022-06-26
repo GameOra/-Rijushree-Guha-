399ae494e010ff5f5bdb9cbb8a370d3b5cdca721
GameOra <106124753+GameOra@users.noreply.github.com>
2022-06-26 22:30:52 +0000

    MOOSE demonstration missions [skip ci]

--- db/migrate/20171009101455_rolify_create_roles.rb
+++ db/migrate/20171009101455_rolify_create_roles.rb
@@ -12,4 +12,6 @@ class RolifyCreateRoles < ActiveRecord::Migration
       t.references :role
     end
 
-    add_index(:roles,
+    add_index(:roles, :name)
+    add_index(:roles, [ :name, :resource_type, :resource_id ])
+    add_index(:users_
