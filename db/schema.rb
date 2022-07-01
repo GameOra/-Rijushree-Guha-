6ae072c4da0abff6d9b05fe22848f3e47ecddaf3
GameOra <106124753+GameOra@users.noreply.github.com>
2022-07-01 20:02:00 +0000

    removed old readme

--- db/schema.rb
+++ db/schema.rb
@@ -1,1 +1,3 @@
-# This file is auto-generated from the current state of the databas
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# increme
