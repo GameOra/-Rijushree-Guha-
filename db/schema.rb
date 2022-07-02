ce4aaa88150fcd7c98d939326f57cd2b996620d2
GameOra <106124753+GameOra@users.noreply.github.com>
2022-07-02 07:09:30 +0000

    add vendor

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
-# increme
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.r
