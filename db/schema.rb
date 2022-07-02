a43e5f59ca66ee675236b2989c48e957bfcf230d
GameOra <106124753+GameOra@users.noreply.github.com>
2022-07-02 12:15:16 +0000

    Copyright year.

--- db/schema.rb
+++ db/schema.rb
@@ -2,4 +2,5 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.r
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the applica
