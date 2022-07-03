b2f259870785311ee88db4417b47882bd73a5fd0
GameOra <106124753+GameOra@users.noreply.github.com>
2022-07-03 15:45:30 +0000

    Update pytest from 4.3.0 to 4.3.1

--- db/schema.rb
+++ db/schema.rb
@@ -8,4 +8,8 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended that you check this file in
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20171009101455) do
+
+  # These a
