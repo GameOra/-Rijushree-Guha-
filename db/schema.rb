ab342782b9ea3c1a7d1ddf2c04f31ae90805871f
GameOra <106124753+GameOra@users.noreply.github.com>
2022-07-15 18:24:36 +0000

    Add VERSION file

--- db/schema.rb
+++ db/schema.rb
@@ -13,4 +13,8 @@
 ActiveRecord::Schema.define(version: 20171009101455) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_exten
+  enable_extension "plpgsql"
+
+  create_table "hangars", force: :cascade do |t|
+    t.integer  "number"
+    t.d
