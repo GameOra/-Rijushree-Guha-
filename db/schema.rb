4e103ec62833ea9a086b75eb31a80b5fe0b6aa49
GameOra <106124753+GameOra@users.noreply.github.com>
2022-07-23 15:04:03 +0000

    Weekly update

--- db/schema.rb
+++ db/schema.rb
@@ -34,4 +34,6 @@ ActiveRecord::Schema.define(version: 20171009101455) do
     t.string   "name"
     t.decimal  "wieght"
     t.integer  "hangar_id"
- 
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.
