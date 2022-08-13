37ecaa12426724991b822f91e0f7585ffae70695
GameOra <106124753+GameOra@users.noreply.github.com>
2022-08-13 17:34:10 +0000

    Remove unneeded class

--- db/schema.rb
+++ db/schema.rb
@@ -55,4 +55,6 @@ ActiveRecord::Schema.define(version: 20171009101455) do
     t.integer  "user_id"
     t.integer  "product_id"
     t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: fals
+    t.datetime "updated_at",       null: false
+    t.index ["product_id"], name: "index_transactions_on_product_id", using: :btree
+    t.index
