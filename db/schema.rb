abe4632ee206216fad6bcbe22d200327770c4c77
GameOra <106124753+GameOra@users.noreply.github.com>
2022-08-14 10:39:28 +0000

    Forward port zendframework/zf2#3883

--- db/schema.rb
+++ db/schema.rb
@@ -60,4 +60,6 @@ ActiveRecord::Schema.define(version: 20171009101455) do
     t.index ["user_id"], name: "index_transactions_on_user_id", using: :btree
   end
 
-  create_table "users
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.stri
