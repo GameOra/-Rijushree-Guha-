df72148c82a94971fcedd4d38067b653b8328f8c
GameOra <106124753+GameOra@users.noreply.github.com>
2022-08-18 13:03:58 +0000

    Fix missing import.

--- db/schema.rb
+++ db/schema.rb
@@ -64,4 +64,6 @@ ActiveRecord::Schema.define(version: 20171009101455) do
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
-    t.date
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",     
