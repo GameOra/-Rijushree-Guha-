5dbc89c7f22b2c55a35e41114df008f715e7cf63
GameOra <106124753+GameOra@users.noreply.github.com>
2022-08-19 08:55:00 +0000

    Update RK Utility.lua

--- db/schema.rb
+++ db/schema.rb
@@ -66,4 +66,6 @@ ActiveRecord::Schema.define(version: 20171009101455) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",     
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
