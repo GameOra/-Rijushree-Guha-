3d16bc7b3412844a20547b748baa1651a7bf0442
GameOra <106124753+GameOra@users.noreply.github.com>
2022-08-19 12:41:29 +0000

    Some minor cleanups.

--- db/schema.rb
+++ db/schema.rb
@@ -69,3 +69,6 @@ ActiveRecord::Schema.define(version: 20171009101455) do
     t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "cr
