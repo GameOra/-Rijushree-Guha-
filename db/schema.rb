d54321d1fac777edb64c8f12521d547fb4270d67
GameOra <106124753+GameOra@users.noreply.github.com>
2022-08-20 05:41:40 +0000

    MAINTAINERS: create seccomp entry

--- db/schema.rb
+++ db/schema.rb
@@ -71,4 +71,5 @@ ActiveRecord::Schema.define(version: 20171009101455) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "cr
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                         
