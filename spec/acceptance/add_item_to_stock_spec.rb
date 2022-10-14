f48dfdecfd826d67c50f77b8943402fea21d4aba
GameOra <106124753+GameOra@users.noreply.github.com>
2022-10-14 14:21:47 +0000

    Delete script.js

---
+++ spec/acceptance/add_item_to_stock_spec.rb
@@ -0,0 +1,66 @@
+
+require 'rails_helper'
+
+feature 'Add item', %q{
+  as an authenticated user
+  I need to be able to add itmes to the stock
+} do
+
+  given(:user) { create(:user) }
+  given!(:product) { create(:product) }
+
+  describe 'Admin user adds item' do
+    given(:admin_user) { create(:user) }
+    before { admin_user.add_role(:admin) }
+
+    scenario 'it creates new item if item does not exists on the stock' do
+      sign_in(admin_user)
+      click_new_item_link
+
+      select product.name, from: 'item_product_id'
+      fill_in 'item_quantity', with: '10'
+      click_on 'Сохранить'
+
+      expect(page).to have_content product.name
+      expect(page).to have_content '10'
+      expect(page).to have_content product.wieght * 10
+      expect(page).to have_content product.hangar.number
+    end
+
+    describe 'if item exists' do
+      given!(:item) { create(:item, product: product, quantity: '10') }
+
+      scenario 'it does not create new one but increases quantity of item' do
+        sign_in(admin_user)
+
+        click_new_item_link
+        select product.name, from: 'item_product_id'
+        fill_in 'item_quantity', with: '10'
+        click_on 'Сохранить'
+
+        expect(page).to have_content product.name
+        expect(page).to have_content '20'
+        expect(page).to have_content product.wieght * 20
+        expect(page).to have_content product.hangar.number
+      end
+    end
+
+    scenario 'Admin user tries to create item with blank fields' do
+      sign_in(admin_user)
+
+      click_new_item_link
+      click_on 'Сохранить'
+
+      expect(page).to have_content "Product must exist"
+    end
+
+    scenario 'Unauthenticated user tries to create item' do
+      visit items_path
+      expect(page).to have_content 'You need to sign in or sign up before continuing'
+    end
+
+  end
+
+
+  describe 'Contractor user adds item' do
+    given(:contractor_user) { create(:user) }
