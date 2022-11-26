22504a388e74e55b596797e5b737a7732103a4d0
GameOra <106124753+GameOra@users.noreply.github.com>
2022-11-26 19:06:54 +0000

    Another attempt.

--- spec/acceptance/items_list_spec.rb
+++ spec/acceptance/items_list_spec.rb
@@ -4,4 +4,6 @@ feature 'List items', %q{
   In order to find the item in stock
   I need to be able to view list of items
 } do
-    given(:user) { create(:user
+    given(:user) { create(:user) }
+    given(:products) { create_list(:product, 2) }
+    given!(:first_item) { create(:item, p
