f2b6d5a34033c3768c9e68a54079ad199a600d3d
GameOra <106124753+GameOra@users.noreply.github.com>
2022-12-07 15:40:07 +0000

    Successful AU test.

--- spec/acceptance/view_item_spec.rb
+++ spec/acceptance/view_item_spec.rb
@@ -4,4 +4,8 @@ feature 'View item', %q{
   I need to be able to view item
 } do
   given(:user) { create(:user) }
-  given(:product) { create(:product)
+  given(:product) { create(:product) }
+  given(:item) { create(:item, product: product, quantity: 3) }
+
+  context 'Admin user' do
+  
