fd01bc05cfafeddda0cb2d15200094767a8dd2e2
GameOra <106124753+GameOra@users.noreply.github.com>
2023-01-14 07:08:01 +0000

    Commit package-lock.json

--- spec/acceptance/view_product_spec.rb
+++ spec/acceptance/view_product_spec.rb
@@ -4,4 +4,7 @@ feature 'View product', %q{
   I need to be able to view product
 } do
   given(:user) { create(:user) }
-  # given(:product) { create(:pro
+  # given(:product) { create(:product_with_comments) }
+  given(:product) { create(:product) }
+
+  context 'Aut
