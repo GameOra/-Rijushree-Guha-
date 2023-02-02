6e4abb69beb329a229d3aa843165b78cc8937704
GameOra <106124753+GameOra@users.noreply.github.com>
2023-02-02 06:40:37 +0000

    :boom:

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -82,3 +82,86 @@ RSpec.describe ItemsController, type: :controller do
       end
 
       it 'assigns requested item to @item' do
+        expect(assigns(:item)).to eq item
+      end
+
+      it 'renders show view' do
+        expect(response).to render_template :show
+      end
+    end
+
+    context 'contractor tries to view item' do
+      sign_in_user
+
+      before do
+        @user.add_role(:contractor)
+        get :show, params: { id: item }
+      end
+
+      it 'assigns requested item to @item' do
+        expect(assigns(:item)).to eq item
+      end
+
+      it 'renders show view' do
+        expect(response).to render_template :show
+      end
+    end
+
+    context 'seller tries to view item' do
+      sign_in_user
+
+      before do
+        @user.add_role(:seller)
+        get :show, params: { id: item }
+      end
+
+      it 'assigns requested item to @item' do
+        expect(assigns(:item)).to eq item
+      end
+
+      it 'renders show view' do
+        expect(response).to render_template :show
+      end
+    end
+  end
+
+  describe 'GET #new' do
+    context 'admin tries to create item' do
+      sign_in_user
+      before do
+        @user.add_role(:admin)
+        get :new
+      end
+      it 'assigns a new Item to @item' do
+        expect(assigns(:item)).to be_a_new(Item)
+      end
+
+      it 'renders new view' do
+        expect(response).to render_template :new
+      end
+    end
+
+    context 'seller tries to create item' do
+      sign_in_user
+      before do
+        @user.add_role(:seller)
+        get :new
+      end
+      it 'assigns a new Item to @item' do
+        expect(assigns(:item)).not_to be_a_new(Item)
+      end
+
+      it 'renders new view' do
+        expect(response).to redirect_to products_path
+      end
+    end
+
+    context 'contractor tries to create item' do
+      sign_in_user
+      before do
+        @user.add_role(:contractor)
+        get :new
+      end
+      it 'assigns a new Item to @item' do
+        expect(assigns(:item)).to be_a_new(Item)
+      end
