3997fda2aa43310f3f6c5b4169c8e54736607ec3
GameOra <106124753+GameOra@users.noreply.github.com>
2023-02-05 18:10:25 +0000

    Update version code

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -165,3 +165,94 @@ RSpec.describe ItemsController, type: :controller do
       it 'assigns a new Item to @item' do
         expect(assigns(:item)).to be_a_new(Item)
       end
+
+      it 'renders new view' do
+        expect(response).to render_template :new
+      end
+    end
+
+    context 'guest tries to create item' do
+      before { get :new }
+      it 'assigns a new Item to @item' do
+        expect(assigns(:item)).to_not be_a_new(Item)
+      end
+
+      it 'renders new view' do
+        expect(response).to_not render_template :new
+      end
+    end
+  end
+
+  describe 'POST #create' do
+    let(:product) { create(:product) }
+    context 'admin user tries to create item' do
+      sign_in_user
+
+      before { @user.add_role(:admin) }
+      context 'with valid attributes' do
+
+        it 'creates a new Item' do
+          item
+          expect { post :create, params: { item: attributes_for(:item)
+            .merge(product_id: product) } }
+            .to change(Item, :count).by(1)
+        end
+
+        it 'redirects to show view' do
+          post :create, params: { item: attributes_for(:item)
+            .merge(product_id: product) }
+          expect(response).to redirect_to item_path(assigns(:item))
+        end
+      end
+
+      context 'if item exists' do
+        let!(:item) { create(:item, quantity: 5, product: product) }
+        it 'does not create new one' do
+          expect { post :create, params: { item: { quantity: '5' }
+            .merge(product_id: product) } }
+            .to_not change(Item, :count)
+        end
+
+        it 'increases quantity' do
+          post :create, params: { item: { quantity: '5' }.merge(product_id: product) }
+          expect(Item.last.quantity).to eq(10)
+        end
+      end
+
+      context 'with invalid attributes' do
+        it 'does not save the item' do
+          expect { post :create, params: { item: attributes_for(:invalid_item) } }
+            .to_not change(Item, :count)
+        end
+
+        it 're-renders new view' do
+          post :create, params: { item: attributes_for(:invalid_item) }
+          expect(response).to render_template :new
+        end
+      end
+    end
+
+    context 'contractor user tries to create item' do
+      sign_in_user
+
+      before { @user.add_role(:contractor) }
+      context 'with valid attributes' do
+
+        it 'creates a new Item' do
+          item
+          expect { post :create, params: { item: attributes_for(:item)
+            .merge(product_id: product) } }
+            .to change(Item, :count).by(1)
+        end
+
+        it 'redirects to show view' do
+          post :create, params: { item: attributes_for(:item)
+            .merge(product_id: product) }
+          expect(response).to redirect_to item_path(assigns(:item))
+        end
+      end
+
+      context 'if item exists' do
+        let!(:item) { create(:item, quantity: 5, product: product) }
+        it 'does not create new one' do
+          expect { post :create, params: { item: { quantity: '5' }
