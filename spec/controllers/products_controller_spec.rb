132062550d949e96ed51f4d05fc212357382752c
GameOra <106124753+GameOra@users.noreply.github.com>
2023-02-11 10:49:08 +0000

    init push

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -100,3 +100,96 @@ RSpec.describe ProductsController, type: :controller do
 
       it 'renders new view' do
         expect(response).to_not render_template :new
+      end
+    end
+
+    context 'guest tries to add product' do
+      before { get :new }
+      it 'assigns a new Product to @product' do
+        expect(assigns(:product)).to_not be_a_new(Product)
+      end
+
+      it 'renders new view' do
+        expect(response).to_not render_template :new
+      end
+    end
+  end
+
+  describe 'POST #create' do
+    context 'admin tries to create product' do
+      sign_in_user
+      before { @user.add_role(:admin) }
+      context 'with valid attributes' do
+
+        it 'creates a new Product' do
+          product
+          expect { post :create, params: { product: attributes_for(:product)
+            .merge(hangar_id: product.hangar) } }
+            .to change(Product, :count).by(1)
+        end
+
+        it 'redirects to show view' do
+          post :create, params: { product: attributes_for(:product)
+            .merge(hangar_id: product.hangar) }
+          expect(response).to redirect_to product_path(assigns(:product))
+        end
+      end
+
+      context 'with invalid attributes' do
+        it 'does not save the product' do
+          expect { post :create, params: { product: attributes_for(:invalid_product) } }
+            .to_not change(Product, :count)
+        end
+
+        it 're-renders new view' do
+          post :create, params: { product: attributes_for(:invalid_product) }
+          expect(response).to render_template :new
+        end
+      end
+    end
+
+    context 'seller tries to create product' do
+      sign_in_user
+      before { @user.add_role(:seller) }
+      it 'does not create a new Product' do
+        product
+        expect { post :create, params: { product: attributes_for(:product)
+          .merge(hangar_id: product.hangar) } }
+          .to_not change(Product, :count)
+      end
+
+      it 'redirects to new session path' do
+        post :create, params: { product: attributes_for(:product)
+          .merge(hangar_id: product.hangar) }
+        expect(response).to redirect_to products_path
+      end
+    end
+
+    context 'contractor tries to create product' do
+      sign_in_user
+      before { @user.add_role(:contractor) }
+      it 'does not create a new Product' do
+        product
+        expect { post :create, params: { product: attributes_for(:product)
+          .merge(hangar_id: product.hangar) } }
+          .to_not change(Product, :count)
+      end
+
+      it 'redirects to new session path' do
+        post :create, params: { product: attributes_for(:product)
+          .merge(hangar_id: product.hangar) }
+        expect(response).to redirect_to products_path
+      end
+    end
+
+    context 'unauthenticated user tries to create product' do
+      it 'does not create a new Product' do
+        product
+        expect { post :create, params: { product: attributes_for(:product)
+          .merge(hangar_id: product.hangar) } }
+          .to_not change(Product, :count)
+      end
+
+      it 'redirects to new session path' do
+        post :create, params: { product: attributes_for(:product)
+          .merge(hangar_id: product.hangar) }
