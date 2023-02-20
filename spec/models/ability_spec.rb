f5d1400df2131eecea476f9eb6c0d6e4e0439ef5
GameOra <106124753+GameOra@users.noreply.github.com>
2023-02-20 21:19:55 +0000

    Temp Commit

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -4,4 +4,7 @@ describe Ability do
   subject(:ability) { Ability.new(user) }
 
   describe 'for guest' do
-    
+    let(:user) { nil }
+
+    it { should_not be_able_to :read, :all }
+    it { should_not be_able_to :mana
