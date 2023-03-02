0498dcd87c519384e9ecdb9e7e987bb1d773e142
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-02 21:10:03 +0000

    Added a file

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,4 +5,6 @@ RSpec.describe Product, type: :model do
   it { should have_many(:items) }
   it { should have_many(:transactions) }
 
-  it { s
+  it { should validate_presence_of(:name) }
+  it { should validate_uniqueness_of(:name) }
+  it { should valid
