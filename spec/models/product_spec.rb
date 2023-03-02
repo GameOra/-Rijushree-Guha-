171105b2c3d25c8c05370a52e6b3a8a8dc44c473
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-02 11:01:16 +0000

    51

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  it { should belong
+  it { should belong_to(:hangar) }
+  it { should have_many(:items) }
+  it { should have_many(:transactions) }
+
+  it { s
