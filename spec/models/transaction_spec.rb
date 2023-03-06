48f51afb5d80816a0ced51c772173fccd70ca797
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-06 22:16:01 +0000

    Add image to readme

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
   it { should belong_to(:user) }
- 
+  it { should belong_to(:product) }
+
+  it { should validate_presence_of(:transaction_date) 
