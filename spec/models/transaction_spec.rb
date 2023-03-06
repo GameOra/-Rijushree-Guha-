3cd6ca5ae34ef0d6266b5631fba0fe65204edb4d
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-06 23:06:29 +0000

    Updated the hstspreload feedstock.

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -4,4 +4,6 @@ RSpec.describe Transaction, type: :model do
   it { should belong_to(:user) }
   it { should belong_to(:product) }
 
-  it { should validate_presence_of(:transaction_date) 
+  it { should validate_presence_of(:transaction_date) }
+  it { should validate_presence_of(:quantity) }
+  it { should validate_numericality_of(:qua
