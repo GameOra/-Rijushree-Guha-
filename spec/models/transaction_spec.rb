50baa17dae0243547ffafd12212007982fcf1dc4
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-08 08:35:38 +0000

    Renamed repository

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -6,4 +6,5 @@ RSpec.describe Transaction, type: :model do
 
   it { should validate_presence_of(:transaction_date) }
   it { should validate_presence_of(:quantity) }
-  it { should validate_numericality_of(:qua
+  it { should validate_numericality_of(:quantity) }
+end
