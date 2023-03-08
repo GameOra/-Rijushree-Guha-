336421b81ec08471b8e330c7a77106d08b2a96ad
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-08 22:48:30 +0000

    Auto commit without commit message.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-ENV[
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+# Prevent database truncati
