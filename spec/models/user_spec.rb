f2a3230ad5fd713de81c174baa7f127f6e988e83
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-08 15:04:22 +0000

    mpi: Fix NULL ptr dereference in mpi_powm() [ver #3]

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :password }
   it { should validate_presence_of :name }
 
-  let
+  let(:user) { create(:user) }
+
+  describe '#set_role' do
+    before do
+      user.add_role(:sell
