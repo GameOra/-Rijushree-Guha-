7bcc2db3ba82ac2d6fa013c242d46e6b97a7bbb0
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-08 11:48:23 +0000

    Update media

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { should have_many(:transactions) }
   it { should validate_presence_of :email }
-  it { should validat
+  it { should validate_presence_of :password }
+  it { should validate_presence_of :name }
+
+  let
