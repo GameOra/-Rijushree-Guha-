2362b1f0264cc265d088772b7e8cc29b40d386e6
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-09 13:09:32 +0000

    units

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,4 +8,5 @@ require 'rspec/rails'
 require 'cancan/matchers'
 # Add additional requires below this line. Rails is not loaded until this point!
 
-# Requires supporting ruby files with cu
+# Requires supporting ruby files with custom matchers and macros, etc, in
+# spec/support/ and its subdirectories. Files matching
