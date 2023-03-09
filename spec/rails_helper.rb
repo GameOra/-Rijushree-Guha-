cfdb899a401ce981ce82003365d745ccc4dd4a6d
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-09 14:02:11 +0000

    updating #metasploit-git# %REBUILD%

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,4 +9,5 @@ require 'cancan/matchers'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching
+# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
+# run as spec files by default. This means that files in spec/support that 
