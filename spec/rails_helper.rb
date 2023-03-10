a6eadc5c7880ece6df19a866755eb25a5c3f8a33
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-10 11:21:25 +0000

    Remove unnecessary method.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,4 +13,7 @@ require 'cancan/matchers'
 # run as spec files by default. This means that files in spec/support that end
 # in _spec.rb will both be required and run as specs, causing the specs to be
 # run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure th
+# end with _spec.rb. You can configure this pattern with the --pattern
+# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
+#
+# The f
