477c3b40c01ab3d849a2ded96a1c7ac4f479c266
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-12 19:24:02 +0000

    Fixed doxygen warnings.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,4 +30,6 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, type: :controller
-  config.extend ControllerMacros, 
+  config.extend ControllerMacros, type: :controller
+  config.include AcceptanceHelper, type: :feature
+  # Remove this line if you're not
