8ecb89c25775e62e178058054e5881397e114f8d
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-12 21:25:50 +0000

    Update the .gitignore file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,3 +34,6 @@ RSpec.configure do |config|
   config.include AcceptanceHelper, type: :feature
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within 
