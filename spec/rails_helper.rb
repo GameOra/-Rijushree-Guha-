95001ce9c8d6f8c3bd66a1a1832c93d47c9150ad
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-13 07:38:35 +0000

    Update flexberry-orm

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,4 +36,6 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within 
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_
