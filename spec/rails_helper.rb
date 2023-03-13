1c2d4a7b94cf7ef5668ac93e5507a67d95db6014
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-13 19:58:23 +0000

    README: Fix typo

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -57,4 +57,8 @@ RSpec.configure do |config|
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
-  # arbitra
+  # arbitrary gems may also be filtered via:
+  # config.filter_gems_from_backtrace("gem name")
+end
+
+Shoulda::Matchers.configure
