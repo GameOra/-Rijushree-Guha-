e5b59a6f0a3e302b378253691ec13d50a66faa6c
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-13 11:39:25 +0000

    splash page

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -42,4 +42,7 @@ RSpec.configure do |config|
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/
+  # `post` in specs under `spec/controllers`.
+  #
+  # You can disable this behaviour by removing the line below, and instead
+  # explicitly tag your sp
