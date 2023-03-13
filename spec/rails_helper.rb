34a1641f66c7c10e75555dec77db6afca9f25f33
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-13 22:18:01 +0000

    fixes links

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,9 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 end
 
-Shoulda::Matchers.configure
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
