e68c849bc3b70680eebeffd002e83c6dad1fa2d3
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-14 20:22:08 +0000

    Added more docs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,4 +6,5 @@
 #
 # Given that it is always loaded, you are encouraged to keep this file as
 # light-weight as possible. Requiring heavyweight dependencies from this file
-# will add 
+# will add to the boot time of your test suite on EVERY test run, even for an
+# individual file that may not nee
