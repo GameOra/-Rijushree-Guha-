8ea09880bda791e922f460dbb203a4ac18fbf522
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-15 16:27:46 +0000

    Btrfs: fall back to non-inline if we don't have enough space

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,3 +10,7 @@
 # individual file that may not need all of that loaded. Instead, consider making
 # a separate helper file that requires the additional dependencies and performs
 # the additional setup, and require it from the spec files that actually need
+# it.
+#
+# See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+RSpec.configure do |config
