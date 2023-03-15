300d6b7d7fdaa195a3be8a25fe4675cc26360c66
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-15 13:32:48 +0000

    Sigh...

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,4 +8,5 @@
 # light-weight as possible. Requiring heavyweight dependencies from this file
 # will add to the boot time of your test suite on EVERY test run, even for an
 # individual file that may not need all of that loaded. Instead, consider making
-# a separate helper file that requires the addi
+# a separate helper file that requires the additional dependencies and performs
+# the additional setup, and require it from the spec files that actually need
