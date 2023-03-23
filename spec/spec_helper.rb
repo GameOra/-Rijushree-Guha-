98d0256bf82abb24a0f103cf068526e10cbe2a6f
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-23 08:28:33 +0000

    Fix compile issue

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,4 +20,6 @@ RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
-    # defined using `chain`, e.g.
+    # defined using `chain`, e.g.:
+    #     be_bigger_than(2).and_smaller_than(4).description
+    #     # => "be bigger tha
