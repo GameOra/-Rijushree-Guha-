f2adf8b0e7e60adcbbf2cf621371a8d122fef01f
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-24 14:51:45 +0000

    Update schema.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,3 +39,5 @@ RSpec.configure do |config|
 
   # This option will default to `:apply_to_host_groups` in RSpec 4 (and will
   # have no way to turn it off -- the option exists only for backwards
+  # compatibility in RSpec 3). It causes shared context metadata to be
+  # inherited by the m
