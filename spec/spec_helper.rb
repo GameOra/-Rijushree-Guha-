cfa57429aa67fe1ca545a06b43fde16fffb584b0
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-24 18:04:21 +0000

    hp_accel: Ignore the error from lis3lv02d_poweron() at resume

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,4 +41,7 @@ RSpec.configure do |config|
   # have no way to turn it off -- the option exists only for backwards
   # compatibility in RSpec 3). It causes shared context metadata to be
   # inherited by the metadata hash of host groups and examples, rather than
-  # triggering implicit auto-inclusion in groups with matching 
+  # triggering implicit auto-inclusion in groups with matching metadata.
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+# The set
