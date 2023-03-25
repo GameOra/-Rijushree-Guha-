019bab6615847e72a6c403577f0760df0810e0e8
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-25 16:04:05 +0000

    bridge: Pull ip header into skb->data before looking into ip header.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -50,4 +50,6 @@ RSpec.configure do |config|
   # This allows you to limit a spec run to individual examples or groups
   # you care about by tagging them with `:focus` metadata. When nothing
   # is tagged with `:focus`, all examples get run. RSpec also provides
-  # aliases for `it`, `describe`, and `context` tha
+  # aliases for `it`, `describe`, and `context` that include `:focus`
+  # metadata: `fit`, `fdescribe` and `fcontext`, respectively.
+  co
