d5d65590bfc2383664ca6f24db54e895aa75e7f8
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-25 20:33:53 +0000

    Regenerate gemspec for version 0.4.4

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -55,4 +55,5 @@ RSpec.configure do |config|
   config.filter_run_when_matching :focus
 
   # Allows RSpec to persist some state between runs in order to support
- 
+  # the `--only-failures` and `--next-failure` CLI options. We recommend
+  # you configure your
