47a263b3a0de45cf8fa5ef58e38e14337cb6083d
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-25 20:37:25 +0000

    basic server

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -56,4 +56,5 @@ RSpec.configure do |config|
 
   # Allows RSpec to persist some state between runs in order to support
   # the `--only-failures` and `--next-failure` CLI options. We recommend
-  # you configure your
+  # you configure your source control system to ignore this file.
+  config.example_status_persistence_file_path = "s
