09b3b20eb9a9da4240cce8b3b90e1eb2b80d036c
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-28 10:40:05 +0000

    Add existing files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -63,3 +63,4 @@ RSpec.configure do |config|
   # recommended. For more details, see:
   #   - http://rspec.info/blog/2012/06/rspecs-new-expectation-syntax/
   #   - http://www.teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
+  #   - http://rspec.info/blog/2014/05/notable-changes-in-rspec-3/#zero-monkey-patching-mod
