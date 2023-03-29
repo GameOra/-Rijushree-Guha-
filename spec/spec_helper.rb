598a39a2ead02e4af63e93c09dabd9dfed41713e
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-29 20:54:23 +0000

    Version 1.1r43

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -83,4 +83,8 @@ RSpec.configure do |config|
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
-  # the se
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = :random
+
+  # See
