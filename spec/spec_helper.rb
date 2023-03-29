88fe145187af0ca8e7fb50a6a0d954ca6a4f4c7d
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-29 12:03:17 +0000

    mdss: Add newline to VSYNC timestamp

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -68,4 +68,6 @@ RSpec.configure do |config|
 
   # Many RSpec users commonly either run the entire suite or an individual
   # file, and it's useful to allow more verbose output when running an
-  # individu
+  # individual spec file.
+  if config.files_to_run.one?
+    # Use the documentation formatter for de
