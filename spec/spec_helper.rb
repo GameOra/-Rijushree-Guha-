b37ba0460f7709849bab4d54f90f2adef3756ec8
GameOra <106124753+GameOra@users.noreply.github.com>
2023-03-29 19:27:12 +0000

    Translated using Translate.Akeeba (French)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -81,4 +81,6 @@ RSpec.configure do |config|
   # particularly slow.
   config.profile_examples = 10
 
-  # Run specs in random order to surface order dependencies. 
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the se
