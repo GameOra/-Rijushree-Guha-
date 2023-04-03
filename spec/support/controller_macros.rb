1ad2fbf13860cdd0491662f10400afbe235d5c9a
GameOra <106124753+GameOra@users.noreply.github.com>
2023-04-03 22:37:22 +0000

    I'm too lazy to comment this

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -2,4 +2,8 @@ module ControllerMacros
   def sign_in_user
     before do
       @user = create(:user)
- 
+      @request.env['devise_mapping'] = Devise.mappings[:user]
+      sign_in @user
+    end
+  end
+end
